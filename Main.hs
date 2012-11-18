de17288f184746b2f5a2c7337ea9366ac6399bef
Jeremy Shaw <jeremy@seereason.com>
2012-11-18 23:08:30 +0000

    clckwrks-plugin-bugs now (mostly?) working

--- Main.hs
+++ Main.hs
@@ -6,10 +6,12 @@ import Clckwrks.Admin.Template
 import Clckwrks.Monad
 import Clckwrks.Server
 import Clckwrks.Plugin
+import Clckwrks.Bugs.Plugin
 import Control.Applicative ((<$>))
 import Control.Monad.Trans
 import Theme
 import qualified Data.Map as Map
+import Data.Text (Text)
 import qualified Data.Text as Text
 import Happstack.Server
 import Web.Plugin.Core
@@ -28,17 +30,19 @@ clckwrksConfig = ClckwrksConfig
     , clckStaticDir       = "../clckwrks/static"
     , clckTopDir          = Nothing
     , clckEnableAnalytics = False
-    , clckInitHook        = initHook
+    , clckInitHook        = initHook ""
     }
 
 main :: IO ()
 main = simpleClckwrks clckwrksConfig
 
-initHook :: ClckState
+initHook :: Text
+         -> ClckState
          -> ClckwrksConfig ClckURL
          -> IO (ClckState, ClckwrksConfig ClckURL)
-initHook clckState cc =
+initHook baseURI clckState cc =
     do let p = plugins clckState
-       initPlugin p "" clckPlugin
+       initPlugin p baseURI clckPlugin
+       initPlugin p baseURI bugsPlugin
        setTheme p (Just theme)
        return (clckState, cc)

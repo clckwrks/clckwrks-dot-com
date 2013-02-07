b1d6829067fa3caf347c9031f478f015e9a59f27
Jeremy Shaw <jeremy@seereason.com>
2013-02-07 23:39:07 +0000

    work on updated to clckwrks 0.15

--- Main.hs
+++ Main.hs
@@ -7,10 +7,12 @@ import Clckwrks.Server           (simpleClckwrks)
 import Clckwrks.Plugin           (clckPlugin)
 import Clckwrks.Bugs.Plugin      (bugsPlugin)
 import Clckwrks.Media.Plugin     (mediaPlugin)
+import Clckwrks.Page.Plugin      (pagePlugin)
+import Clckwrks.Page.URL         (PageURL(..))
 import Control.Applicative       ((<$>))
 import Data.Text                 (Text)
 import "clckwrks-theme-clckwrks" Theme (theme)
-import Web.Plugins.Core          (addHandler, initPlugin, setTheme)
+import Web.Plugins.Core          (Plugin(..), addHandler, getPluginRouteFn, initPlugin, setTheme)
 import System.Environment        (getArgs)
 
 ------------------------------------------------------------------------------
@@ -20,12 +22,13 @@ import System.Environment        (getArgs)
 clckwrksConfig :: ClckwrksConfig
 clckwrksConfig = ClckwrksConfig
     { clckHostname        = "localhost"
-    , clckPort            = 8000
     , clckHidePort        = False
-    , clckJQueryPath      = ""
-    , clckJQueryUIPath    = ""
-    , clckJSTreePath      = ""
-    , clckJSON2Path       = ""
+    , clckPort            = 8000
+    , clckTLS             = Nothing      -- disable TLS by default
+    , clckJQueryPath      = "../jquery"  -- directory containing 'jquery.js'
+    , clckJQueryUIPath    = ""           -- directory containing 'jquery.js'
+    , clckJSTreePath      = "../jstree"  -- directory containing 'jquery.jstree.js'
+    , clckJSON2Path       = "../json2"   -- directory containing 'json2.js'
     , clckTopDir          = Nothing
     , clckEnableAnalytics = False
     , clckInitHook        = initHook
@@ -39,7 +42,7 @@ main :: IO ()
 main =
     do args <- getArgs
        f    <- parseArgs (clckwrksOpts clckwrksConfig) args
-       simpleClckwrks  (f clckwrksConfig)
+       simpleClckwrks =<< f clckwrksConfig
 
 initHook :: Text
          -> ClckState
@@ -48,18 +51,21 @@ initHook :: Text
 initHook baseURI clckState cc =
     do let p = plugins clckState
        addHandler p "blog" blogHandler
-       initPlugin p baseURI clckPlugin
-       initPlugin p baseURI bugsPlugin
-       initPlugin p baseURI mediaPlugin
+       initPlugin p "" clckPlugin
+       initPlugin p "" pagePlugin
+       initPlugin p "" bugsPlugin
+       initPlugin p "" mediaPlugin
        setTheme p (Just theme)
        return (clckState, cc)
 
 blogHandler :: ClckPlugins -> [Text] -> ClckT ClckURL (ServerPartT IO) Response
-blogHandler _plugins [] =
-    do blogURL <- showURL Blog
-       seeOther blogURL (toResponse ())
-blogHandler _plugins ["atom.xml"] =
-    do atomURL <- showURL AtomFeed
-       seeOther atomURL (toResponse ())
-blogHandler _plugins _ =
-    do notFound (toResponse ())
+blogHandler plugins paths =
+    do (Just showPageURL) <- getPluginRouteFn plugins (pluginName pagePlugin)
+       case paths of
+         [] ->
+            do let blogURL = showPageURL Blog []
+               seeOther blogURL (toResponse ())
+         ["atom.xml"] ->
+             do let atomURL = showPageURL AtomFeed []
+                seeOther atomURL (toResponse ())
+         _ -> notFound (toResponse ())

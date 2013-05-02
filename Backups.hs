49a8b76ea6d68d1ce87f47193911c6a985729703
David Fox <dsf@seereason.com>
2013-05-02 15:32:52 +0000

    Update the backup script for archive-1.4

--- Backups.hs
+++ Backups.hs
@@ -1,5 +1,13 @@
 module Main where
 
+import Network.URI (URIAuth(..))
 import System.Archive.Site (BackupTarget(..), backup)
 
-main = backup (BackupTarget {app = "clckwrks-dot-com-production", user = "upload", host = "seereason.com", keep = 50})
+main = backup (BackupTarget
+               { app = "clckwrks-dot-com-production"
+               , auth = URIAuth { uriUserInfo = "upload@"
+                                , uriRegName = "clckwrks.com"
+                                , uriPort = ""}
+               , keep = 50
+               , localTop = "/home/autobuilder/backups"
+               , remoteTop = "/srv" })

02dfeb9b36bb20e08b8a8f51f170b4344f76d389
Jeremy Shaw <jeremy@n-heptane.com>
2016-12-19 16:41:35 -0600

    0.3.12.1 - allow clckwrks 0.24

--- default.nix
+++ default.nix
@@ -5,16 +5,16 @@
 }:
 mkDerivation {
   pname = "clckwrks-dot-com";
-  version = "0.3.11";
+  version = "0.3.12.1";
   src = ./.;
   isLibrary = false;
   isExecutable = true;
-  buildDepends = [
+  executableHaskellDepends = [
     base clckwrks clckwrks-plugin-media clckwrks-plugin-page
     clckwrks-theme-clckwrks containers happstack-server hsp mtl network
     text web-plugins
   ];
-  buildTools = [ hsx2hs ];
+  executableToolDepends = [ hsx2hs ];
   homepage = "http://www.clckwrks.com/";
   description = "clckwrks.com";
   license = stdenv.lib.licenses.bsd3;
--- shell.nix
+++ shell.nix
@@ -1,25 +1,37 @@
-with (import <nixpkgs> {}).pkgs;
-let pkg = haskellngPackages.callPackage
-            ({ mkDerivation, base, clckwrks
-             , clckwrks-plugin-media, clckwrks-plugin-page
-             , clckwrks-theme-clckwrks, containers, happstack-server, hsp
-             , hsx2hs, mtl, network, stdenv, text, web-plugins, magic
-             }:
-             mkDerivation {
-               pname = "clckwrks-dot-com";
-               version = "0.3.10";
-               src = ./.;
-               isLibrary = false;
-               isExecutable = true;
-               buildDepends = [
-                 base clckwrks clckwrks-plugin-media
-                 clckwrks-plugin-page clckwrks-theme-clckwrks containers
-                 happstack-server hsp mtl network text web-plugins
-               ];
-               buildTools = [ pandoc ];
-               homepage = "http://www.clckwrks.com/";
-               description = "clckwrks.com";
-               license = stdenv.lib.licenses.bsd3;
-             }) {};
+{ nixpkgs ? import <nixpkgs> {}, compiler ? "default" }:
+
+let
+
+  inherit (nixpkgs) pkgs;
+
+  f = { mkDerivation, base, clckwrks, clckwrks-plugin-media
+      , clckwrks-plugin-page, clckwrks-theme-clckwrks, containers
+      , happstack-server, hsp, hsx2hs, mtl, network, stdenv, text
+      , web-plugins
+      }:
+      mkDerivation {
+        pname = "clckwrks-dot-com";
+        version = "0.3.12";
+        src = ./.;
+        isLibrary = false;
+        isExecutable = true;
+        executableHaskellDepends = [
+          base clckwrks clckwrks-plugin-media clckwrks-plugin-page
+          clckwrks-theme-clckwrks containers happstack-server hsp mtl network
+          text web-plugins
+        ];
+        executableToolDepends = [ hsx2hs ];
+        homepage = "http://www.clckwrks.com/";
+        description = "clckwrks.com";
+        license = stdenv.lib.licenses.bsd3;
+      };
+
+  haskellPackages = if compiler == "default"
+                       then pkgs.haskellPackages
+                       else pkgs.haskell.packages.${compiler};
+
+  drv = haskellPackages.callPackage f {};
+
 in
-  pkg.env
+
+  if pkgs.lib.inNixShell then drv.env else drv
